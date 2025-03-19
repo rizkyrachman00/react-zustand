b4c0fb49b296a07bac8c61c43b085c0a753d9404
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2025-03-19 08:18:55 +0700

    feat: layouting homePage & uploadPage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div className="p-9 w-full justify-center flex flex-col text-center">
+      <h1 className="text-3xl font-bold">Selamat Datang</h1>
+      <p className="text-xl">Basic Global State Using Zustand</p>
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
--- src/pages/UploadPage.tsx
+++ src/pages/UploadPage.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const UploadPage = () => {
-  return <div>UploadPage</div>;
+  return (
+    <div className="p-4 flex flex-col items-center justify-center gap-y-5">
+      <h1 className="text-3xl font-bold">Upload Page</h1>
+      <button className="py-3 text-lg px-4 border rounded-lg bg-slate-400 transition duration-150 hover:bg-blue-400">
+        Unggah Data
+      </button>
+    </div>
+  );
 };
 
 export default UploadPage;
