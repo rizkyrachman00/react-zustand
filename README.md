8270fb9ce658f92910a9873f5258f841682a2ed7
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2025-03-19 07:09:37 +0700

    feat: init tailwind config

---
+++ README.md
@@ -0,0 +1,4 @@
+https://medium.com/@akhshyganesh/the-right-way-to-setup-react-and-tailwind-css-with-vite-ae5027f57dda
+
+npm install -D tailwindcss@3 postcss autoprefixer
+npx tailwind init -p
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="p-4 mt-2 bg-red-400 w-full flex justify-center">
+        <p className="w-20 bg-black text-white p-2 text-center">Halo dunia</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
