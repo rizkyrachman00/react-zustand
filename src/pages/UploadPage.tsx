876d9d89944b168e075e524d34dd7df1a8574cae
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2025-03-19 08:33:44 +0700

    feat: add mock API and local state for upload handling

--- src/pages/UploadPage.tsx
+++ src/pages/UploadPage.tsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { mockApiCall } from "../api";
 
 const UploadPage = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleUploadClick = async () => {
+    try {
+      setLoading(true);
+      await mockApiCall();
+    } catch (e) {
+      alert(`Sever error : ${e}`);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="p-4 flex flex-col items-center justify-center gap-y-5">
       <h1 className="text-3xl font-bold">Upload Page</h1>
-      <button className="py-3 text-lg px-4 border rounded-lg bg-slate-400 transition duration-150 hover:bg-blue-400">
+      <button
+        disabled={loading}
+        onClick={handleUploadClick}
+        className="py-3 text-lg px-4 border rounded-lg bg-slate-400 text-white transition duration-150 disabled:opacity-50"
+      >
         Unggah Data
       </button>
     </div>
